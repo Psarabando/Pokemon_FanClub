ef4e409ed9ac76c35617ce7271519377508a83e5
Psarabando <pedro.saraujo@sptech.school>
2023-11-21 02:07:08 -0300

    Atualizando a modelagem lógica, a criação das tabelas no mySQL e mudando alguns detalhes na página de batalha do site

--- Banco de Dados/pokemon_fanClub.sql
+++ Banco de Dados/pokemon_fanClub.sql
@@ -1,10 +1,14 @@
+-- Criação e uso da Database Pokemon
+
 CREATE DATABASE Pokemon;
 USE Pokemon;
+
+-- Criação das tabelas
 
 CREATE TABLE Usuario (
 idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 nomeUsuario VARCHAR(45) NOT NULL,
-idade INT,
+idade INT NOT NULL,
 senha VARCHAR(45) NOT NULL
 );
 
@@ -12,7 +16,7 @@ CREATE TABLE Email (
 idEmail INT AUTO_INCREMENT,
 email VARCHAR(45) UNIQUE NOT NULL,
 fkUsuario INT,
-CONSTRAINT fkUsuario FOREIGN KEY (fkUsuario)
+CONSTRAINT fkUsuarioEmail FOREIGN KEY (fkUsuario)
 	REFERENCES Usuario(idUsuario),
 PRIMARY KEY(idEmail, fkUsuario)
 );
@@ -29,11 +33,23 @@ PRIMARY KEY(idPontos, fkUsuario)
 
 CREATE TABLE Pokemon (
 idPokemon INT PRIMARY KEY AUTO_INCREMENT,
-nomePokemon VARCHAR(45),
+nomePokemon VARCHAR(45) NOT NULL,
 tipagem1 VARCHAR(45) NOT NULL,
 	CONSTRAINT chkTipagem1 CHECK (tipagem1 IN ('Planta', 'Fogo', 'Água', 'Veneno')),
 tipagem2 VARCHAR(45),
 	CONSTRAINT chkTipagem2 CHECK (tipagem2 IN ('Planta', 'Fogo', 'Água', 'Veneno'))
 );
+
+CREATE TABLE PokemonInicial (
+fkUsuario INT,
+CONSTRAINT fkUsuarioPokemonInicial FOREIGN KEY (fkUsuario)
+	REFERENCES Usuario(idUsuario),
+fkPokemon INT,
+CONSTRAINT fkPokemon FOREIGN KEY (fkPokemon)
+	REFERENCES Pokemon(idPokemon),
+PRIMARY KEY (fkUsuario, fkPokemon)
+);
+
+-- Comandos SQL
 
 DROP DATABASE pokemon;
